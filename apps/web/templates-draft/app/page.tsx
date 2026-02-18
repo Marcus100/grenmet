7bbbc023c9569137f14e4899bd8f8bb2aedbe203
Eugine Whint <euginegnd@gmail.com>
2026-02-18 15:15:35 -0400

    Added hello world!

--- apps/web/templates-draft/app/page.tsx
+++ apps/web/templates-draft/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 bg-zinc-50 sm:px-6 sm:py-16">
       <div className="mx-auto max-w-lg">
+        <p>Hello World! </p>
         <WeatherDashboard />
       </div>
       <div className="print:page-break-after-always mx-auto box-border h-[297mm] max-h-[297mm] min-h-[297mm] w-[210mm] min-w-[210mm] max-w-[210mm] overflow-hidden border">
